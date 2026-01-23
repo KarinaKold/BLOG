f9b69e99e854a3eaca824c632853ee4ea1901ff2
Karina Koldaeva <karina.mila@yandex.ru>
2026-01-23 18:32:59 +0300

    Реализовать основные компоненты

---
+++ blog-project/src/components/header/components/logo/Logo.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { Icon } from '../../../../components';
+
+const LargeText = styled.div`
+	font-size: 48px;
+	font-weight: 600;
+	line-height: 48px;
+	margin-top: 17px;
+`;
+
+const SmallText = styled.div`
+	font-size: 18px;
+	font-weight: bold;
+`;
+
+const LogoContainer = ({ className }) => (
+	<div className={className}>
+		<Icon id="fa-code" size="70px" margin="0 10px 0 0" />
+		<div>
+			<LargeText>Блог</LargeText>
+			<SmallText>веб-разработчика</SmallText>
+		</div>
+	</div>
+);
+
+export const Logo = styled(LogoContainer)`
+	display: flex;
+	margin-top: -14px;
+`;
